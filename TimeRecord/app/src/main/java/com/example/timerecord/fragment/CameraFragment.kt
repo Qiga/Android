b683918ccf89cb4189accd7c9fb97e71686ca07c
Qiga <qgam12123@gmail.com>
2023-11-02 13:09:55 +0900

    cameraX previewView 설정

--- TimeRecord/app/src/main/java/com/example/timerecord/fragment/CameraFragment.kt
+++ TimeRecord/app/src/main/java/com/example/timerecord/fragment/CameraFragment.kt
@@ -3,7 +3,6 @@ package com.example.timerecord.fragment
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.ContentValues
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Build
@@ -14,9 +13,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import com.example.timerecord.databinding.FragmentCameraBinding
+import com.google.common.util.concurrent.ListenableFuture
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -54,9 +58,7 @@ class CameraFragment : Fragment() {
     ) { isGranted ->
         if (isGranted) {
             //권환이 허용되었을 때
-            Log.d("permission", "true")
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            getCameraLauncher.launch(intent)
+            setCameraProvider()
         } else {
             //권한 허용 x
             showPermissionPopup()
@@ -67,6 +69,7 @@ class CameraFragment : Fragment() {
         requestPermissionLauncher.launch(CAMERA_PERMISSION)
     }
 
+    //권한요청을 위한 알람 Dialog ( 거부 시 )
     private fun showPermissionPopup() {
         AlertDialog.Builder(requireContext()).setTitle("권한이 필요합니다")
             .setMessage("앱에서 필요한 권한을 허용하시겠습니까").setPositiveButton("네") { _, _ ->
@@ -82,8 +85,7 @@ class CameraFragment : Fragment() {
                 requireContext(), CAMERA_PERMISSION
             ) == PackageManager.PERMISSION_GRANTED -> {
                 Log.d("permission", "true")
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                getCameraLauncher.launch(intent)
+                setCameraProvider()
             }
 
             shouldShowRequestPermissionRationale(CAMERA_PERMISSION) -> {
@@ -91,12 +93,40 @@ class CameraFragment : Fragment() {
             }
 
             else -> {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                getCameraLauncher.launch(intent)
+                startCameraPermissionRequest()
             }
         }
     }
 
+    /**
+     * CameraX를 활용한 카메라 설정
+     */
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+
+    private fun setCameraProvider(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        checkUseAble()
+    }
+    private fun checkUseAble(){
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder().build()
+        val cameraSelector : CameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+        preview.setSurfaceProvider(binding.previewView.getSurfaceProvider())
+        var camera = cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview)
+    }
+
+    /**
+     * 기본 카메라 서비스
+     */
+    //기본 카메라를 통해서 이미지 캡쳐를 하기 위한 launcher
     private val getCameraLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
             if(result.resultCode == Activity.RESULT_OK){
@@ -112,6 +142,7 @@ class CameraFragment : Fragment() {
             }
         }
 
+    //기본 카메라로 bitmap 이미지를 가져왔을 때 디바이스에 저장하기 위한 메서드
     private fun saveImageToAppDirectory(imageBitmap: Bitmap) {
         val timestamp =
             SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREAN).format(Date())
@@ -119,7 +150,7 @@ class CameraFragment : Fragment() {
         val imageFileName = "img_${timestamp}"
         val contentValues = ContentValues()
         contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imageFileName)
-        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/webp")
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
 
         val contentResolver = requireContext().contentResolver
         val imageUri = contentResolver.insert(
@@ -134,53 +165,5 @@ class CameraFragment : Fragment() {
             // 이제 사진이 앱의 전용 디렉토리에 저장되었습니다.
         }
     }
-//    private val getCameraContent =
-//        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-//            if (result.resultCode == Activity.RESULT_OK) {
-//                val data = result.data
-//
-//
-//
-//                    val format =
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) Bitmap.CompressFormat.WEBP_LOSSY else Bitmap.CompressFormat.WEBP
-//
-//                    //thread 작업으로 block되는 것을 방지 위함
-//                    CoroutineScope(Dispatchers.IO).launch {
-//                        try {
-//                            val file = withContext(Dispatchers.IO) {
-//                                File.createTempFile("jpeg_${timestamp}", ".jpg")
-//                            }
-//
-//                            withContext(Dispatchers.IO) {
-//                                FileOutputStream(file).use { result ->
-//                                    bmp!!.compress(
-//                                        format, 100, result
-//                                    )
-//                                }
-//                            }
-//                        } catch (e: Exception) {
-//                            Log.e("이미지 오류", "bitmap 오류")
-//                        }
-//                    }
-//                    bmp?.recycle()
-//                }
-//            }
-//        }
-//    private fun startCaptureLauncher(){
-//        //파일을 분류하여 저장하기 위함
-//        val timestamp: String =
-//            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.KOREA).format(Date())
-//
-//        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1001)
-//        } else {
-//            val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-//            // storageDir을 사용하여 작업을 수행
-//        }
-//        val file = File.createTempFile("jpg_${timestamp}", ".jpg")
-//
-//        val intent = Intent(MediaStore.ACTION_IMSimAGE_CAPTURE)
-//        getCameraContent.launch(intent)
-//    }
 
 }
